e92b59e9deb7113fb6771ed42864252b5b55879d
Eric Torunski <etorunski@gmail.com>
2017-09-12 18:33:19 -0400

    Initial commit

---
+++ app/src/main/java/com/example/etorunski/inclassexamples_17f/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.etorunski.inclassexamples_17f;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+}
